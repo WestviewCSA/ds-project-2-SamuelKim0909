fead404ea7a4a394f5fd9aa383e203bcd058a0d5
1957196 <1957196@01-18102.pusd.dom>
2025-03-10 13:51:29 -0700

    made the test cases

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,9 +1,23 @@
 
 public class p2 {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("p2");
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
