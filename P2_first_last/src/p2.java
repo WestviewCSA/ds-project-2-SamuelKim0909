5a83b5ec0fc8dbc1426fdd9ad39fc482e15fb129
1957196 <1957196@01-18102.pusd.dom>
2025-03-11 14:06:13 -0700

    basic setup done by Mr.David. (reading the file)

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -1,12 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class p2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("p2");
+		
 	}
 	
-	
-	
+	public static void readMap(String filename) {
+		
+		try {
+			File file = new File(filename);
+			Scanner scanner = new Scanner(file);
+			
+			int numRows = scanner.nextInt();
+			int numCols = scanner.nextInt();
+			int numRooms = scanner.nextInt();
+			
+			int rowIndex = 0;
+			
+			while(scanner.hasNextLine()) {
+				String row = scanner.nextLine();
+				if(row.length()>0) {
+					for(int i = 0; i<numCols && i< row.length(); i++) {
+						char el = row.charAt(i);
+						Tile obj = new Tile(rowIndex, i, el);
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+		
+		
+		
+		
+	}
 	
 	
 	
