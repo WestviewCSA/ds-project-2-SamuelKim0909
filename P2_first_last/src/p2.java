994a96bcf647ce0525ecc1ad0b4bdebce2f2e318
1957196 <1957196@01-18102.pusd.dom>
2025-03-14 13:31:15 -0700

    created search method. now I can try recursive.

--- P2_first_last/src/p2.java
+++ P2_first_last/src/p2.java
@@ -6,21 +6,29 @@ import java.util.Scanner;
 public class p2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("test case 1");
 		readMap("test case 1");
 		
 	}
-	
+	static int numRow = 0;
+	static int numCol = 0;
+	static int numRoom = 0;
+	static Queue<Tile> queue = null;
+	static Map map = null;
 	public static void readMap(String filename) {
 			
 		try {
 			File file = new File(filename);
 			Scanner scanner = new Scanner(file);
-			Queue<Tile> queue = new Queue<Tile>();
+			queue = new Queue<Tile>();
 			int numRows = scanner.nextInt();
 			int numCols = scanner.nextInt();
 			int numRooms = scanner.nextInt();
-			Map map = new Map(new Tile[numRows][numCols][numRooms]);
+			numRow = numRows;
+			numCol = numCols;
+			numRoom = numRooms;
+			map = new Map(new Tile[numRows][numCols][numRooms]);
 			int rowIndex = 0;
 			int roomNum = 0;
 			int aRow=0;
@@ -87,7 +95,6 @@ public class p2 {
 					queue.enqueue(West);
 				}
 				queue.toString();
-				
 			//}
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
@@ -96,6 +103,40 @@ public class p2 {
 	
 	
 	
+	public static Tile search(Tile temp) {
+		Tile North = null;
+		Tile South = null;
+		Tile West = null;
+		Tile East = null;
+		if(temp.getRow()>=1) {
+			North = map.getTile(temp.getRow()-1,temp.getCol(),temp.getRoom());
+			System.out.println(North.getType());
+		}
+		if(temp.getRow()< numRow) {
+			South = map.getTile(temp.getRow()+1,temp.getCol(),temp.getRoom());
+			System.out.println(South.getType());
+		}
+		if(temp.getCol() >= 1) {
+			West = map.getTile(temp.getRow(),temp.getCol()-1,temp.getRoom());
+			System.out.println(West.getType());
+		}
+		if(temp.getCol()<numCol) {
+			East = map.getTile(temp.getRow(),temp.getCol()+1,temp.getRoom());
+			System.out.println(East.getType());
+		}
+		
+		if(North.getType()=='.') {
+			queue.enqueue(North);
+		}else if(South.getType()=='.') {
+			queue.enqueue(South);
+		}else if(East.getType()=='.') {
+			queue.enqueue(East);
+		}else if(West.getType()=='.') {
+			queue.enqueue(West);
+		}
+		queue.toString();
+		return temp;
+	}
 	
 	
 	
